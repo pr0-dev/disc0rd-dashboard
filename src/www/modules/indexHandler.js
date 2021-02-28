a823eafd6be622dc263981b61ac7bb847a7ce4ca
NullDev (Chris) <shadow@nulldev.org>
2021-02-28 23:09:22 +0100

    moved index route

---
+++ src/www/modules/indexHandler.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// =========================== //
+// = Copyright (c) TheShad0w = //
+// =========================== //
+
+// Dependencies
+let fetch = require("node-fetch");
+
+// Utils
+let log = require("../../utils/logger");
+let config = require("../../utils/configHandler").getConfig();
+
+let { getPr0Account, getPr0Name } = require("./pr0Helpers");
+
+module.exports = async function(req, res){
+    let pr0 = !req.session.user
+        ? null
+        : await getPr0Account((await getPr0Name(req.session.user.id)).name);
+
+    // @ts-ignore
+    let guilds = !req.session.access_token ? null : await (await fetch("https://discordapp.com/api/users/@me/guilds", {
+        headers: {
+            authorization: `${req.session.token_type} ${req.session.access_token}`
+        }
+    })).json();
+
+    // @ts-ignore
+    let guildInfo = !guilds || !req.session.access_token ? null : await (await fetch(`https://discordapp.com/api/guilds/${config.auth.server_id}/members/${req.session.user.id}`, {
+        headers: {
+            authorization: `Bot ${config.auth.bot_token}`
+        }
+    })).json();
+
+    return res.render("pages/index", {
+        "routeTitle": "Dashboard",
+        "route": req.path,
+        "user": req.session.user || null,
+        "guilds": guilds,
+        "dc": guildInfo,
+        "config": config,
+        "csrfToken": req.csrfToken(),
+        "log": log,
+        "pr0": pr0,
+        "status_code": 200
+    });
+};
--- src/www/router.js
+++ src/www/router.js
@@ -4,9 +4,6 @@
 // = Copyright (c) TheShad0w = //
 // =========================== //
 
-// Dependencies
-let fetch = require("node-fetch");
-
 // Utils
 let log = require("../utils/logger");
 let config = require("../utils/configHandler").getConfig();
@@ -14,7 +11,6 @@ let config = require("../utils/configHandler").getConfig();
 // Services
 let getRoutes = require("./services/getRoutes");
 let robotsHandler = require("./modules/robotsHandler");
-let { getPr0Account, getPr0Name } = require("./modules/pr0Helpers");
 let callbackHandler = require("./modules/callBackHandler");
 
 // Routes
@@ -23,6 +19,8 @@ let unsetNick = require("./modules/api/unsetNick");
 let getRoles = require("./modules/api/getRoles");
 let setRole = require("./modules/api/setRole");
 let unsetRole = require("./modules/api/unsetRole");
+let indexHandler = require("./modules/indexHandler");
+let notFoundHandler = require("./modules/404Handler");
 
 const { clientId, scopes, redirectUri } = config.webserver.auth;
 const authorizeUrl = `https://discordapp.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scopes.join("%20")}`;
@@ -32,37 +30,7 @@ let routes;
 let checkAuth = (req, res, next) => (!req.session.user) ? res.redirect("/") : next();
 
 module.exports = function(app, client){
-    app.get("/", async(req, res) => {
-        let pr0 = !req.session.user
-            ? null
-            : await getPr0Account((await getPr0Name(req.session.user.id)).name);
-
-        // @ts-ignore
-        let guilds = !req.session.access_token ? null : await (await fetch("https://discordapp.com/api/users/@me/guilds", {
-            headers: {
-                authorization: `${req.session.token_type} ${req.session.access_token}`
-            }
-        })).json();
-
-        // @ts-ignore
-        let guildInfo = !guilds || !req.session.access_token ? null : await (await fetch(`https://discordapp.com/api/guilds/${config.auth.server_id}/members/${req.session.user.id}`, {
-            headers: {
-                authorization: `Bot ${config.auth.bot_token}`
-            }
-        })).json();
-
-        return res.render("pages/index", {
-            "routeTitle": "Dashboard",
-            "route": req.path,
-            "user": req.session.user || null,
-            "guilds": guilds,
-            "dc": guildInfo,
-            "config": config,
-            "csrfToken": req.csrfToken(),
-            "log": log,
-            "pr0": pr0
-        });
-    });
+    app.get("/", async(req, res) => indexHandler(req, res));
 
     app.get("/callback", (req, res) => callbackHandler(req, res));
     app.get("/auth", (req, res) => res.redirect(!!req.session.user ? "/" : authorizeUrl));
@@ -77,13 +45,7 @@ module.exports = function(app, client){
 
     app.get("/robots.txt", robotsHandler);
 
-    // Errors
-    app.get("*", (req, res) => {
-        res.render("errors/404", {
-            "routeTitle": "Nicht gefunden :(",
-            "route": req.path,
-        });
-    });
+    app.get("*", (req, res) => notFoundHandler(req, res));
 
     routes = getRoutes(app);
     for (let i in routes){
