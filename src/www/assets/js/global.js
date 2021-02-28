ee9cdfdfeee395eb50751e1efa8c6d8f813c0403
NullDev (Chris) <shadow@nulldev.org>
2021-02-28 21:39:46 +0100

    fetching logic

--- src/www/assets/js/global.js
+++ src/www/assets/js/global.js
@@ -4,19 +4,54 @@
 // = Copyright (c) TheShad0w = //
 // =========================== //
 
-/* eslint-disable no-var, no-use-before-define, consistent-return */
+/* eslint-disable no-var, no-use-before-define, consistent-return, quotes */
 
-let handleNickChange = async function(){
+let handleNickChange = async function(e, SA, force = false){
     let res = await (await fetch(
-        this.checked
+        (force || e.target.checked)
             ? "/nick/set"
             : "/nick/unset"
     )).json();
 
-    console.log(res);
+    return SA.fire({
+        position: "bottom-start",
+        title: res.error === 0 ? res.message : "Fehler beim aktualisieren des Nicknames",
+        // background: "#9AA3B5",
+        toast: true,
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true
+    });
 };
 
-($ => {
+let getRoles = async function(SA, $){
+    let res = await (await fetch("/roles/get")).json();
+
+    if (res.error !== 0){
+        return SA.fire({
+            position: "bottom-start",
+            title: "Fehler beim laden der Rollen",
+            // background: "#9AA3B5",
+            toast: true,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true
+        });
+    }
+
+    $(".user-info.user-info-dc.user-info-dc-rollen").empty().addClass("grid-style");
+
+    res.roles.forEach((r, i) => {
+        $(".user-info.user-info-dc.user-info-dc-rollen").append(
+            '<div class="role">' +
+            '    <input type="checkbox" name="role-' + i + '" id="role-' + i + '"' + (r.on_user ? " checked" : "") + '>' +
+            '    <label for="role-' + i + '">' + r.role + '</label>' +
+            '</div>'
+        );
+    });
+};
+
+(($, SA) => {
     $("a").attr("target", function(){
         if (this.host && this.host !== location.host) return "_blank";
     });
@@ -26,7 +61,11 @@ let handleNickChange = async function(){
     });
 
     $(document).ready(function(){
-        $("#sync-nick").on("change", handleNickChange);
+        if ($(".logged-in-inner").hasClass("logged-in")){
+            $("#sync-nick").on("change", e => handleNickChange(e, SA));
+            $("#resync-nick").on("click", e => handleNickChange(e, SA, true));
+            getRoles(SA, $);
+        }
     });
 // @ts-ignore
-})($ || window.jQuery);
+})($ || window.jQuery, swal || window.swal || window.Swal);
