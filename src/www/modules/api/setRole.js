1c1c7eca41ae217dedafa16b5f32f2c87602eac9
NullDev (Chris) <shadow@nulldev.org>
2021-02-28 22:41:28 +0100

    added role setter

---
+++ src/www/modules/api/setRole.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// =========================== //
+// = Copyright (c) TheShad0w = //
+// =========================== //
+
+let config = require("../../../utils/configHandler").getConfig();
+
+module.exports = async function(req, res, client){
+    let response = {
+        error: !!req.session.user ? 0 : 1,
+        status: !!req.session.user ? 200 : 403,
+        message: !!req.session.user ? "Rolle wurde hinzugefügt." : "Nicht authorisiert."
+    };
+
+    if (!!req.session.user){
+        if (!req.query.role){
+            response.message = "Keine Rolle angegeben.";
+            response.error = 1;
+        }
+        else if (!config.rollen_auswahl.includes(decodeURIComponent(req.query.role))) {
+            response.message = "Diese Rolle darf nicht gesetzt werden.";
+            response.error = 1;
+        }
+        else {
+            try {
+                client.guilds.cache
+                    .get(config.auth.server_id).members.cache
+                    .get(req.session.user.id).roles
+                    .add(
+                        client.guilds.cache
+                            .get(config.auth.server_id).roles.cache
+                            .find(r => r.name === decodeURIComponent(req.query.role))
+                    );
+            }
+            catch (e){
+                response.error = 1;
+                response.status = 500;
+                response.message = String(e);
+            }
+        }
+    }
+
+    return res.set({
+        "Content-Type": "application/json; charset=utf-8"
+    }).status(response.status).send(response);
+};
