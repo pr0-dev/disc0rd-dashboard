db9c5742dc7caf0450a01dbbe7f1e65f3d0bf276
NullDev (Chris) <shadow@nulldev.org>
2021-03-09 09:43:57 +0100

    Added invite command

---
+++ src/commands/invite.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// =========================== //
+// = Copyright (c) TheShad0w = //
+// =========================== //
+
+/**
+ * Send the user an invite to the server
+ *
+ * @param {import("discord.js").Client} client
+ * @param {import("discord.js").Message} message
+ * @param {Array} args
+ * @param {Function} callback
+ * @returns {any} callback
+ */
+exports.run = (client, message, args, callback) => {
+    message.react("✉");
+    message.author.send("Invite Link: https://discord.pr0gramm.com");
+
+    return callback();
+};
+
+exports.description = "Sendet einen Invite link für den Server.";
