1ba71bd540c3c9b7719d5a603f2ae156034bfb47
Jonas Breuer <jonas.breuer@is-it-fresh.com>
2021-03-28 01:20:37 +0100

    fully ported to tailwindcss and fixed a few bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@
 module.exports = {
     purge: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "dark": "rgb(47, 49, 54)",
+                "discord-blue": "#7289DA",
+                "discord-blue-dark": "#5566a3",
+                "light": "#36393f"
+            }
+        },
     },
     variants: {},
     plugins: [],
