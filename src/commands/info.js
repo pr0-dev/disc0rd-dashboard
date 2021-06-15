cd753d4d5b82811e960316d2c0aab33a0e69b387
NullDev <shadow@nulldev.org>
2021-06-15 08:39:19 +0200

    fetch contributors from github

--- src/commands/info.js
+++ src/commands/info.js
@@ -4,6 +4,19 @@
 // = Copyright (c) TheShad0w = //
 // =========================== //
 
+let fetch = require("node-fetch").default;
+
+/**
+ * Get all contributors from GitHub
+ *
+ * @return {Promise<String>}
+ */
+let getContributors = function(){
+    return new Promise(async resolve => resolve((await (await fetch("https://api.github.com/repos/pr0-dev/disc0rd-dashboard/contributors", {
+        headers: { Accept: "application/vnd.github.v3+json" }
+    })).json()).filter(e => e.type === "User").map(e => `<${e.html_url}> (Contributions: ${e.contributions})`).join("\n")));
+};
+
 /**
  * Shows some generic infos
  *
@@ -11,12 +24,14 @@
  * @param {import("discord.js").Message} message
  * @param {Array} args
  * @param {Function} callback
- * @returns {Function} callback
+ * @returns {Promise<Function>} callback
  */
-exports.run = (client, message, args, callback) => {
+exports.run = async(client, message, args, callback) => {
     message.react("✉");
     message.author.send(
         "Programmiert von ShadowByte#1337 (TheShad0w) für den pr0gramm disc0rd (<https://discord.pr0gramm.com>)\n\n" +
+        "Contributions von:\n" +
+        (await getContributors()) + "\n\n" +
         "Eckdaten:\n" +
         "- Programmiersprache: NodeJS\n" +
         "- NodeJS Version: " + process.version + "\n" +
