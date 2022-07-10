73237a49a94170240f43a2980ad9050f32b73bc5
NullDev (Chris) <shadow@nulldev.org>
2022-07-10 03:07:55 +0200

    removed not supported delete reason

--- src/modules/spamWatcher.js
+++ src/modules/spamWatcher.js
@@ -20,14 +20,17 @@ const WATCHER_SET = new Set();
  */
 module.exports = async function(message, client){
     if (/free/gi.test(message.content) && /nitro/gi.test(message.content) && /(https|http|www|com|tk|ml|cf)/gi.test(message.content)){
-        message.delete({ reason: "Possible Nitro Spam Link" });
+        message.delete(/* { reason: "Possible Nitro Spam Link" } */);
 
         if (WATCHER_SET.has(message.author.id)){
             WATCHER_SET.delete(message.author.id);
 
             message.author.send("Du wurdest wegen spam vom pr0gramm Disc0rd verbannt. Wenn du denkst, dass das ein Fehler war, melde dich bitte an ShadowByte#1337");
 
-            (await message.guild.members.fetch(message.author.id)).ban(({ days: 7, reason: "Possible nitro spam detected (second offense)" }));
+            (await message.guild?.members.fetch(message.author.id))?.ban(({
+                days: 7,
+                reason: "Possible nitro spam detected (second offense)",
+            }));
 
             const embed = {
                 embed: {
@@ -35,15 +38,15 @@ module.exports = async function(message, client){
                     description: message.cleanContent + "\n\n\- - - - -",
                     author: {
                         name: `User ${message.author.username} wurde wegen Nitro-Spam gebannt.`,
-                        icon_url: message.author.displayAvatarURL()
+                        icon_url: message.author.displayAvatarURL(),
                     },
                     fields: [
                         { name: "Von User", value: message.author, inline: true },
                         { name: "User Tag", value: message.author.tag, inline: true },
                         { name: "User ID", value: message.author.id, inline: true },
-                        { name: "Datum/Zeit", value: String(moment().format("DD.MM.YYYY HH:mm:ss")), inline: true }
-                    ]
-                }
+                        { name: "Datum/Zeit", value: String(moment().format("DD.MM.YYYY HH:mm:ss")), inline: true },
+                    ],
+                },
             };
 
             client.channels
